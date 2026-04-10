852b3b54012129a8472f90ac74da8788d8a29674
x-Joshe-x <jcrisantoipq@gmail.com>
2026-04-10 10:20:06 -0500

    subiendo ejercicio1 en c#

---
+++ Desktop/pollo/FA SEMA 3/SE3-C/ejercicio1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            string n, c;
+            Console.Write("ingrese el nombre del estudiante: ");
+            n = Console.ReadLine();
+            Console.Write("\ningrese el nombre de la carrera: ");
+            c = Console.ReadLine();
+
+            Console.WriteLine("----------IMPRESION CON +-------------");
+
+            Console.WriteLine(n+", bienvenido a la carrera de "+c);
+            Console.WriteLine("----------IMPRESION CON $-------------");
+            Console.WriteLine($"{n}, bienvenido a la carrera de {c}"); 
+            Console.ReadKey(); 
+        }
+    }
+}
