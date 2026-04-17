76c2ad92c82d8b5d4189203fce4b6dc120f3062d
x-Joshe-x <jcrisantoipq@gmail.com>
2026-04-17 10:34:02 -0500

    actualizacion de semana 4

---
+++ Desktop/pollo/FA SEMA 3/sem3/ejer3.py
@@ -0,0 +1,8 @@
+NUM1 = int(input("Ingrese numero 1: "));
+NUM2 = int(input("Ingrese numero 2: "));
+print(f"\nSuma: {NUM1+NUM2}");
+print(f"\nResta: {NUM1-NUM2}");
+print(f"\nMultiplicacion: {NUM1*NUM2}");
+print(f"\nDivision: {NUM1//NUM2}");
+
+
